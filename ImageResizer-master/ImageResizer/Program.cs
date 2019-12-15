311a2af41b589c61d7b8e12c473101307262edbc
wys035 <58912508+wys035@users.noreply.github.com>
2019-12-16 00:28:12 +0800

    hoemwork

---
+++ ImageResizer-master/ImageResizer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+
+            ImageProcess imageProcess = new ImageProcess();
+
+            imageProcess.Clean(destinationPath);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+
+            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
